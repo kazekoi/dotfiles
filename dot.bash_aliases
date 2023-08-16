65f7e767f861ab0d2bdf3c249dd3f76586488fc5
TK <lumis.eterne.168@gmail.com>
2023-08-16 22:26:42 +0900

    add .gitconfig

---
+++ dot.bash_aliases
@@ -0,0 +1,12 @@
+# Utils
+mkdircd() {
+  mkdir $* && cd $_
+}
+alias mkdircd="mkdircd"
+mkdirpcd() {
+  mkdir -p $* && cd $_
+}
+alias mkdirpcd="mkdirpcd"
+
+# Git
+alias gl="git log -n 10 --oneline --decorate"
--- install.sh
+++ install.sh
@@ -3,7 +3,12 @@
 DIR="$HOME/dotfiles"
 
 mkdir -p $DIR
-ln -sf $DIR/.vimrc ~/.vimrc
-ln -sf $PWD/dot.vimrc $DIR/.vimrc
+
 ln -sf $DIR/.bash_aliases ~/.bash_aliases
 ln -sf $PWD/dot.bash_aliases $DIR/.bash_aliases
+
+ln -sf $DIR/.gitconfig ~/.gitconfig
+ln -sf $PWD/dot.gitconfig $DIR/.gitconfig
+
+ln -sf $DIR/.vimrc ~/.vimrc
+ln -sf $PWD/dot.vimrc $DIR/.vimrc
