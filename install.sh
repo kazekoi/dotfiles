577f87b43e4561637f0e0ea239c44a07c70d91db
TK <lumis.eterne.168@gmail.com>
2023-08-16 22:32:02 +0900

    rm .gitconfig

--- install.sh
+++ install.sh
@@ -7,8 +7,5 @@ mkdir -p $DIR
 ln -sf $DIR/.bash_aliases ~/.bash_aliases
 ln -sf $PWD/dot.bash_aliases $DIR/.bash_aliases
 
-ln -sf $DIR/.gitconfig ~/.gitconfig
-ln -sf $PWD/dot.gitconfig $DIR/.gitconfig
-
 ln -sf $DIR/.vimrc ~/.vimrc
 ln -sf $PWD/dot.vimrc $DIR/.vimrc
