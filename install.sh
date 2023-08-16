3438266c33b429325687d5da9129fd7c610ade5a
TK <lumis.eterne.168@gmail.com>
2023-08-16 18:14:51 +0900

    add .bash_aliases

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ DIR="$HOME/dotfiles"
 mkdir -p $DIR
 ln -sf $DIR/.vimrc ~/.vimrc
 ln -sf $PWD/dot.vimrc $DIR/.vimrc
+ln -sf $DIR/.bash_aliases ~/.bash_aliases
+ln -sf $PWD/dot.bash_aliases $DIR/.bash_aliases
